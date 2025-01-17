beb6f45f52985d57f6effbd7da74acac2094aad8
justcoddev <edisonpaulcrz@gmail.com>
2025-01-17 13:24:05 -0500

    Agregar componente Navbar y botones de navegación con funcionalidad de pestañas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
 				// #### Headings, Call- to - actions, Header Navigation
 				"sans": ['"SF Pro Display"', 'sans-serif'], // Usando la fuente personalizada
 				// #### Body
-				"serif": ['"Open Sans"', "serif"], // Otra fuente para el cuerpo
+				// "serif": ['"Open Sans"', "serif"], // Otra fuente para el cuerpo
 			},
 
 			fontSize: {
@@ -21,6 +21,7 @@ export default {
 			colors: {
 				'hub-title': '#121020',
 				'hub-subtitle': '#888888',
+				'hub-light-bg-2': '#FAFAFA',
 				'hub-border': '#EDEDED',
 				'hub-inactive': '#B0B0B0',
 				'hub-background': '#FFFFFF',
@@ -30,25 +31,6 @@ export default {
 			backgroundImage: {
 				'btn-gradient': 'linear-gradient(to right, #825BDD, #5327BA)', // Gradiente para botones
 			},
-
-			// colors: {
-			// 	// ### Primary
-			// 	"dark-blue-1": "hsl(217, 28%, 15%)", //(intro and email sign up background)
-			// 	"dark-blue-2": "hsl(218, 28%, 13%)", //(main background)
-			// 	"dark-blue-3": "hsl(216, 53%, 9%)", //(footer background)
-			// 	"dark-blue-4": "hsl(219, 30%, 18%)", //(testimonials background)
-			// 	// ### Accent
-			// 	"cyan": "hsl(176, 68%, 64%)",
-			// 	"blue": "hsl(198, 60%, 50%)",
-			// 	"light-red": "hsl(0, 100%, 63%)",
-			// 	// ### Neutral
-			// 	"white-neutral": "hsl(0, 0%, 100%)"
-			// },
-			// // ## Shadows
-			// boxShadow: {
-
-			// 	"testimonial": "0px 0px 10px 1px rgba(0, 0, 0, 0.5)"
-			// },
 		},
 
 	},
