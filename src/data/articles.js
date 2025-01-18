70b3e36e763c3029369b54b72aa8255f176be83e
justcoddev <edisonpaulcrz@gmail.com>
2025-01-18 08:29:50 -0500

    Agregar nuevos componentes de tarjeta y mejorar la funcionalidad de visualización de artículos y proyectos

---
+++ src/data/articles.js
@@ -0,0 +1,38 @@
+export const cardsData = [
+  {
+    id: "doc-1",
+    href: "https://medium.com/article-1",
+    title: "Lorem ipsum dolor sit amet consectetur.",
+    subtitle: "www.medium.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Ícono del aplicativo",
+
+  },
+  {
+    id: "card-2",
+    href: "https://medium.com/article-2",
+    title: "Como começar em UI/UX Design: Guia Prático",
+    subtitle: "www.medium.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Imagen de portafolio",
+
+  },
+  {
+    id: "card-3",
+    href: "https://medium.com/article-1",
+    title: "Princípios de Design Centrado no Usuário",
+    subtitle: "www.medium.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Ícono del proyecto de prueba",
+
+  },
+  {
+    id: "card-4",
+    href: "https://medium.com/article-1",
+    title: "Como começar em UI/UX Design: Guia Prático",
+    subtitle: "www.medium.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Logo de Vercel",
+
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,38 @@
+export const cardsData = [
+  {
+    id: "card-1",
+    href: "https://www.github.com/justcoddev",
+    title: "Aplicativo de Clima",
+    subtitle: "www.github.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Ícono del aplicativo",
+    className: "custom-card-class",
+  },
+  {
+    id: "card-2",
+    href: "https://portfolio.justcoddev.com",
+    title: "Mi Portafolio",
+    subtitle: "portfolio.justcoddev.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Imagen de portafolio",
+    className: "custom-card-class-2",
+  },
+  {
+    id: "card-3",
+    href: "https://example.com",
+    title: "Proyecto de Prueba",
+    subtitle: "www.example.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Ícono del proyecto de prueba",
+    className: "custom-card-class",
+  },
+  {
+    id: "card-4",
+    href: "https://vercel.com",
+    title: "Despliegues con Vercel",
+    subtitle: "vercel.com",
+    imageSrc: "/src/assets/img/my-notion-face-portrait.png",
+    imageAlt: "Logo de Vercel",
+    className: "custom-card-class-3",
+  },
+];
