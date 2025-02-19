9eea4dc9795474fa2b6dfd0c6255bbe5fad4b358
justcoddev <edisonpaulcrz@gmail.com>
2025-02-19 17:56:53 -0500

    Agregar soporte para múltiples idiomas y crear páginas en español

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+  en: {
+    'nav.index': 'Home',
+    'nav.about': 'About',
+    'nav.404': '404-Not-Found',
+  },
+  es: {
+    'nav.index': 'Inicio',
+    'nav.about': 'Acerca de',
+    'nav.404': '404-No-Encontrado',
+  },
+} as const;
