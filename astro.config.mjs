c07b60c239d595fdf81f1560b378b039606c1a5c
chelsea <chelsea.qrt@gmail.com>
2025-05-24 21:14:28 +0800

    add base router

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  base: '/blog/', 
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh'],
